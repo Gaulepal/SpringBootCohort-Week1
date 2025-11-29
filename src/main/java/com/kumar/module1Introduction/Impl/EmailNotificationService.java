693c0dd84b8f2176996784f8232e059106d526e7
gaulepal <gaulepal@gmail.com>
2025-11-29 15:48:02 +0100

    get all the services

--- src/main/java/com/kumar/module1Introduction/Impl/EmailNotificationService.java
+++ src/main/java/com/kumar/module1Introduction/Impl/EmailNotificationService.java
@@ -2,12 +2,11 @@ package com.kumar.module1Introduction.Impl;
 
 import com.kumar.module1Introduction.NotificationService;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 @Component
 @Qualifier("emailQualifier")
-@ConditionalOnProperty(name = "notification.type", havingValue = "email")
+//@ConditionalOnProperty(name = "notification.type", havingValue = "email")
 public class EmailNotificationService implements NotificationService {
   @Override
   public void send(String message) {
--- src/main/java/com/kumar/module1Introduction/Module1IntroductionApplication.java
+++ src/main/java/com/kumar/module1Introduction/Module1IntroductionApplication.java
@@ -1,19 +1,27 @@
 package com.kumar.module1Introduction;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class Module1IntroductionApplication implements CommandLineRunner {
 
   // field Injection is not recommended -> so remove @Autowired //  @Autowired
-  final NotificationService notificationService;
+//  final NotificationService notificationService;
+//
+//  // make the construction this class
+//  public Module1IntroductionApplication(NotificationService notificationService) {
+//    this.notificationService = notificationService;
+//  }
 
-  // make the construction this class
-  public Module1IntroductionApplication(NotificationService notificationService) {
-    this.notificationService = notificationService;
-  }
+  // get all the implementation of the NotificationService
+  @Autowired
+  Map<String, NotificationService> notificationServiceMap = new HashMap<>();
 
   public static void main(String[] args) {
     SpringApplication.run(Module1IntroductionApplication.class, args);
@@ -22,8 +30,14 @@ public class Module1IntroductionApplication implements CommandLineRunner {
   @Override
   public void run(String... args) throws Exception {
 
-    notificationService.send("bonjour!");
+//    notificationService.send("bonjour!");
 //    smsNotificationService.send("bonjour");
+
+    // forEach
+    for (var notificationService : notificationServiceMap.entrySet()) {
+      System.out.println("notification services " + notificationService.getKey());
+      notificationService.getValue().send("namaste!");
+    }
   }
 }
 
