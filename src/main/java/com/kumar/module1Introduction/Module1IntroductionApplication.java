b3124a410766d577d759df693657b410b77e0098
gaulepal <gaulepal@gmail.com>
2025-11-29 12:59:52 +0100

    add Service DI

--- src/main/java/com/kumar/module1Introduction/Module1IntroductionApplication.java
+++ src/main/java/com/kumar/module1Introduction/Module1IntroductionApplication.java
@@ -1,13 +1,24 @@
 package com.kumar.module1Introduction;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Module1IntroductionApplication {
+public class Module1IntroductionApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Module1IntroductionApplication.class, args);
-	}
+  // inject the service with @Autowired
+  @Autowired
+  PaymentService paymentService;
 
+  public static void main(String[] args) {
+    SpringApplication.run(Module1IntroductionApplication.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    // call the payment service
+    paymentService.pay();
+  }
 }
--- src/main/java/com/kumar/module1Introduction/PaymentService.java
+++ src/main/java/com/kumar/module1Introduction/PaymentService.java
@@ -1,5 +1,9 @@
 package com.kumar.module1Introduction;
 
+import org.springframework.stereotype.Service;
+
+//@Component // 👈
+@Service
 public class PaymentService {
   public void pay() {
     System.out.println("Paying...");
